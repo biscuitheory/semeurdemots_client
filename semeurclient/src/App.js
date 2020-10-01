68166fd0efc864b37a1a79ffb8a6743d5d6e9586
Sylvie <sylvie.z.lu@gmail.com>
2020-10-01 13:36:50 +0200

    router created w/ Commander page

--- semeurclient/src/App.js
+++ semeurclient/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Commander from './pages/Commander';
+
 import './styles/App.scss';
 
 function App() {
   return (
-    <div className="App">
-     <p>TEST</p>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/commander" component={Commander} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ semeurclient/src/pages/Commander.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+
+const API = process.env.REACT_APP_API_URL;
+
+const Commander = () => {
+  return (
+    <>
+      <h1>Hello commander !</h1>
+    </>
+  );
+};
+
+export default Commander;
