7322948bb844bd64b365b2854f197662513d63ad
Sylvie <sylvie.z.lu@gmail.com>
2020-10-01 13:09:49 +0200

    fonts update

--- semeurclient/src/App.js
+++ semeurclient/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+     <p>TEST</p>
     </div>
   );
 }
