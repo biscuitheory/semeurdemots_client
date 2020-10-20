e36769c310e431109f59661e576cb3105b78586a
Sylvie <sylvie.z.lu@gmail.com>
2020-10-20 13:05:55 +0200

    update Cart page

--- semeurclient/src/pages/Cart.jsx
+++ semeurclient/src/pages/Cart.jsx
@@ -50,28 +50,56 @@ const Cart = () => {
       <SubHeader title="Panier" />
       {console.log('produit', products)}
       {/* {console.log('fiii', cart)} */}
-
+      <div className="cart__container-products">
+        <div className="cart__container-products-box">
+          <div className="cart__container-products-box-delete"></div>
+          <div className="cart__container-products-box-img">
+            <h5>Produit:</h5>
+          </div>
+          <div className="cart__container-products-box-name"></div>
+          <div className="cart__container-products-box-price">
+            <h5>Prix:</h5>
+          </div>
+          <div className="cart__container-products-box-quantity">
+            <h5>Quantité:</h5>
+          </div>
+          <div className="cart__container-products-box-subtotal">
+            <h5>Sous-total:</h5>
+          </div>
+        </div>
+        {/* <div className="cart__container-products-update">
+        <button>Mettre à jour le panier</button>
+      </div> */}
+      </div>
       {products.map((product, i) => (
         <CartProductRow key={i} {...product} />
       ))}
 
       <div className="cart__container-totalcart">
-        <div className="cart__container-totalcart-subtotall">
-          <h5>Sous-total:</h5>
-          <p>22.00€</p>
-        </div>
-        <div className="cart__container-totalcart-shipping">
-          <span className="cart__container-totalcart-shipping-labels">
+        <h2 className="cart__container-totalcart-title">Total panier</h2>
+        <div className="cart__container-totalcart-box">
+          <div className="cart__container-totalcart-box-subtotal">
+            <h5>Sous-total:</h5>
+            <p>22.00€</p>
+          </div>
+          <div className="cart__container-totalcart-box-shipping">
             <h5>Expédition:</h5>
-            <p>Forfait</p>
-          </span>
-          <p>
-            Les options de livraison seront mises à jour lors de la commande.
-          </p>
+            <span className="cart__container-totalcart-box-shipping-label">
+              <p>Forfait</p>
+              <p>
+                Les options de livraison seront mises à jour lors de la
+                commande.
+              </p>
+            </span>
+          </div>
+          <div className="cart__container-totalcart-box-total">
+            <h5>TOTAL</h5>
+            <p>22.00€</p>
+          </div>
         </div>
       </div>
 
-      <button className="cart__container-confirm-button">
+      <button className="cart__container-confirm-button submit-button">
         Valider la commande
       </button>
     </div>
@@ -85,22 +113,22 @@ function CartProductRow({ dataValues, quantity }) {
       <div className="cart__container-products-box">
         <div className="cart__container-products-box-delete">x</div>
         {/* <div className="cart__container-products-box-img"></div> */}
-        <img src={dataValues.image} alt="product" className="cart__container-products-box-img"></img>
+        <img
+          src={dataValues.image}
+          alt="product"
+          className="cart__container-products-box-img"
+        ></img>
         <div className="cart__container-products-box-name">
-          <h5>Produit:</h5>
           <p>{dataValues.name}</p>
         </div>
         <div className="cart__container-products-box-price">
-          <h5>Prix:</h5>
-          <p>{dataValues.price}</p>
+          <p>{dataValues.price} €</p>
         </div>
         <div className="cart__container-products-box-quantity">
-          <h5>Quantité:</h5>
           <input value={dataValues.id}></input>
         </div>
         <div className="cart__container-products-box-subtotal">
-          <h5>Sous-total:</h5>
-          <p>22.00€</p>
+          <p>{dataValues.price * dataValues.id} €</p>
         </div>
       </div>
       {/* <div className="cart__container-products-update">
