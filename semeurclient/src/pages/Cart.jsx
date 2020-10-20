61ae75a7f5038c821fa8d42f32e5e30a5731310f
Sylvie <sylvie.z.lu@gmail.com>
2020-10-20 15:37:15 +0200

    Update Cart style

--- semeurclient/src/pages/Cart.jsx
+++ semeurclient/src/pages/Cart.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import SubHeader from '../components/SubHeader';
 import axios from 'axios';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
 const API = process.env.REACT_APP_API_URL;
 
 const Cart = () => {
@@ -45,65 +48,82 @@ const Cart = () => {
     allStorage();
   }, []);
 
-  return (
-    <div className="cart__container">
-      <SubHeader title="Panier" />
-      {console.log('produit', products)}
-      {/* {console.log('fiii', cart)} */}
-      <div className="cart__container-products">
-        <div className="cart__container-products-box">
-          <div className="cart__container-products-box-delete"></div>
-          <div className="cart__container-products-box-img">
-            <h5>Produit:</h5>
-          </div>
-          <div className="cart__container-products-box-name"></div>
-          <div className="cart__container-products-box-price">
-            <h5>Prix:</h5>
-          </div>
-          <div className="cart__container-products-box-quantity">
-            <h5>Quantité:</h5>
-          </div>
-          <div className="cart__container-products-box-subtotal">
-            <h5>Sous-total:</h5>
-          </div>
+  if (products <= 0) {
+    return (
+      <div className="cart__container">
+        <SubHeader title="Panier" />
+        <div className="cart__container-products">
+          <p>Votre panier est actuellement vide</p>
         </div>
-        {/* <div className="cart__container-products-update">
+      </div>
+    );
+  }
+  {
+    return (
+      <div className="cart__container">
+        <SubHeader title="Panier" className="subheader__container" />
+        {console.log('produit', products)}
+        {/* {console.log('fiii', cart)} */}
+        <div className="cart__container-products">
+          <div
+            className="cart__container-products-box"
+            style={{ alignItems: 'center' }}
+          >
+            <div className="cart__container-products-box-delete">
+              <FontAwesomeIcon icon={faTrashAlt} />
+            </div>
+            <div className="cart__container-products-box-img">
+              <h5>Produit:</h5>
+            </div>
+            <div className="cart__container-products-box-name"></div>
+            <div className="cart__container-products-box-price">
+              <h5>Prix:</h5>
+            </div>
+            <div className="cart__container-products-box-quantity">
+              <h5>Quantité:</h5>
+            </div>
+            <div className="cart__container-products-box-subtotal">
+              <h5>Sous-total:</h5>
+            </div>
+          </div>
+          {/* <div className="cart__container-products-update">
         <button>Mettre à jour le panier</button>
       </div> */}
-      </div>
-      {products.map((product, i) => (
-        <CartProductRow key={i} {...product} />
-      ))}
+        </div>
+        {products.map((product, i) => (
+          <CartProductRow key={i} {...product} />
+        ))}
 
-      <div className="cart__container-totalcart">
-        <h2 className="cart__container-totalcart-title">Total panier</h2>
-        <div className="cart__container-totalcart-box">
-          <div className="cart__container-totalcart-box-subtotal">
-            <h5>Sous-total:</h5>
-            <p>22.00€</p>
-          </div>
-          <div className="cart__container-totalcart-box-shipping">
-            <h5>Expédition:</h5>
-            <span className="cart__container-totalcart-box-shipping-label">
-              <p>Forfait</p>
-              <p>
-                Les options de livraison seront mises à jour lors de la
-                commande.
-              </p>
-            </span>
-          </div>
-          <div className="cart__container-totalcart-box-total">
-            <h5>TOTAL</h5>
-            <p>22.00€</p>
+        <div className="cart__container-totalcart">
+          <h2 className="cart__container-totalcart-title">Total panier</h2>
+          <div className="cart__container-totalcart-box">
+            <div className="cart__container-totalcart-box-subtotal">
+              <h5>Sous-total:</h5>
+              <p>{}€</p>
+            </div>
+            <div className="cart__container-totalcart-box-shipping">
+              <h5>Expédition:</h5>
+              <span className="cart__container-totalcart-box-shipping-label">
+                <p>Forfait</p>
+                <p>
+                  Les options de livraison seront mises à jour lors de la
+                  commande.
+                </p>
+              </span>
+            </div>
+            <div className="cart__container-totalcart-box-total">
+              <h5>TOTAL</h5>
+              <p>22.00€</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <button className="cart__container-confirm-button submit-button">
-        Valider la commande
-      </button>
-    </div>
-  );
+        <button className="cart__container-confirm-button submit-button">
+          Valider la commande
+        </button>
+      </div>
+    );
+  }
 };
 
 function CartProductRow({ dataValues, quantity }) {
