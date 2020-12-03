a471b52b0f787261f10cd9b706d1103065c96629
Sylvie <sylvie.z.lu@gmail.com>
2020-12-03 23:37:55 +0100

    added logic Orders Page from adminPortal

--- semeurclient/src/pages/adminPortal/Orders.jsx
+++ semeurclient/src/pages/adminPortal/Orders.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API = process.env.REACT_APP_API_URL;
 
 const Orders = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const res = await axios.get(`${API}allorders`);
+      setOrders(res.data);
+      console.log('from orders ', res.data);
+    };
+    fetchOrders();
+  }, []);
+  
   return (
     <>
       <p>Editer les commandes</p>
