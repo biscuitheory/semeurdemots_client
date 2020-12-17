711e39a9f9b547b94179b47cfdd33c3cdd144da0
Sylvie <sylvie.z.lu@gmail.com>
2020-12-17 18:51:46 +0100

    updated Poisson rouge page

--- semeurclient/src/pages/PoissonRouge.jsx
+++ semeurclient/src/pages/PoissonRouge.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PoissonRouge = () => {
   return (
     <div
       className="homepage__container"
       style={{
-        paddingTop: '10em',
+        // paddingTop: '10em',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
+        height: '100vh',
       }}
     >
-      <h2>🚧 Partie du site en chantier 🚧</h2>
-      <p>La partie éditoriale est en cours de réécriture...</p>
-      <p>
+      <h2 style={{ textAlign: 'center' }}>🚧 Partie du site en chantier 🚧</h2>
+      <p style={{ textAlign: 'center' }}>
+        La partie éditoriale est en cours de réécriture...
+      </p>
+      <p style={{ textAlign: 'center' }}>
         Si vous souhaitez tout de même consulter la version bêta du site
         réalisée sur Wordpress, elle est disponible à l&apos;adresse suivante
         :&nbsp;
-        <a href="https://www.semeurdemots.fr">https://www.semeurdemots.fr/</a>
       </p>
-      <p>
-        En attendant, vous pouvez bien entendu visiter la boutique en ligne et
-        passer vos commandes 🛒
+      <a
+        style={{ textAlign: 'center', fontSize: '1.2em', marginBottom: '1em' }}
+        href="https://www.semeurdemots.fr"
+      >
+        https://www.semeurdemots.fr/
+      </a>
+      <p style={{ textAlign: 'center' }}>
+        En attendant, vous pouvez bien entendu visiter la&nbsp;
+        <Link to="/">
+          <button className="submit-button" type="button">
+            Boutique en ligne
+          </button>
+        </Link>
+        &nbsp; et tester la commande de produits 🛒
       </p>
     </div>
   );
